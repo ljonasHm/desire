5150f69a064d0ef9d593e1dab7eb5705b1b0d58b
ljonasHm <shamordinanton@gmail.com>
2021-06-23 22:33:05 +0300

    completed rightside menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './scss/style.scss';
+
+function rightsideMenu() {
+    const menu = document.querySelector('.rightside-menu');
+    const menuButton = document.querySelector('.header__btn');
+    const menuClose = document.querySelector('.rightside-menu__close');
+
+    menuButton.addEventListener('click', () => {
+        menu.classList.remove('rightside-menu--close');
+    });
+    menuClose.addEventListener('click', () => {
+        menu.classList.add('rightside-menu--close');
+    })
+}
+
+rightsideMenu();
